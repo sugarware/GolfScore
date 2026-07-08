16d5d661b2257013aef6b38cf530f5a3b085bafe
sugarware <46047287+sugarware@users.noreply.github.com>
2026-07-08 19:47:05 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,9 @@ const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./apple-touch-icon.png",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./icons/apple-touch-icon.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
